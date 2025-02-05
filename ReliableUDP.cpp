d96bf03802fe935db7ec56fde7fd9dfb4a55136e
Benjamin <benjamin.richardson038@gmail.com>
2025-02-05 14:05:26 -0500

    Revised code. Client now accuretly sends the file size, file MD5 hash, and file name to the server and the server prints it out to the console for testing purposes. At this point in time the file name is actually the file path and still needs to be changed so the right file name is send

--- ReliableUDP.cpp
+++ ReliableUDP.cpp
@@ -1,7 +1,7 @@
 /*
-	Reliability and Flow Control Example
-	From "Networking for Game Programmers" - http://www.gaffer.org/networking-for-game-programmers
-	Author: Glenn Fiedler <gaffer@gaffer.org>
+    Reliability and Flow Control Example
+    From "Networking for Game Programmers" - http://www.gaffer.org/networking-for-game-programmers
+    Author: Glenn Fiedler <gaffer@gaffer.org>
 */
 
 #include <iostream>
@@ -28,317 +28,239 @@ const int PacketSize = 256;
 class FlowControl
 {
 public:
+    FlowControl()
+    {
+        printf("flow control initialized\n");
+        Reset();
+    }
 
-	FlowControl()
-	{
-		printf("flow control initialized\n");
-		Reset();
-	}
+    void Reset()
+    {
+        mode = Bad;
+        penalty_time = 4.0f;
+        good_conditions_time = 0.0f;
+        penalty_reduction_accumulator = 0.0f;
+    }
 
-	void Reset()
-	{
-		mode = Bad;
-		penalty_time = 4.0f;
-		good_conditions_time = 0.0f;
-		penalty_reduction_accumulator = 0.0f;
-	}
+    void Update(float deltaTime, float rtt)
+    {
+        const float RTT_Threshold = 250.0f;
 
-	void Update(float deltaTime, float rtt)
-	{
-		const float RTT_Threshold = 250.0f;
+        if (mode == Good)
+        {
+            if (rtt > RTT_Threshold)
+            {
+                printf("*** dropping to bad mode ***\n");
+                mode = Bad;
+                if (good_conditions_time < 10.0f && penalty_time < 60.0f)
+                {
+                    penalty_time *= 2.0f;
+                    if (penalty_time > 60.0f)
+                        penalty_time = 60.0f;
+                    printf("penalty time increased to %.1f\n", penalty_time);
+                }
+                good_conditions_time = 0.0f;
+                penalty_reduction_accumulator = 0.0f;
+                return;
+            }
 
-		if (mode == Good)
-		{
-			if (rtt > RTT_Threshold)
-			{
-				printf("*** dropping to bad mode ***\n");
-				mode = Bad;
-				if (good_conditions_time < 10.0f && penalty_time < 60.0f)
-				{
-					penalty_time *= 2.0f;
-					if (penalty_time > 60.0f)
-						penalty_time = 60.0f;
-					printf("penalty time increased to %.1f\n", penalty_time);
-				}
-				good_conditions_time = 0.0f;
-				penalty_reduction_accumulator = 0.0f;
-				return;
-			}
+            good_conditions_time += deltaTime;
+            penalty_reduction_accumulator += deltaTime;
 
-			good_conditions_time += deltaTime;
-			penalty_reduction_accumulator += deltaTime;
+            if (penalty_reduction_accumulator > 10.0f && penalty_time > 1.0f)
+            {
+                penalty_time /= 2.0f;
+                if (penalty_time < 1.0f)
+                    penalty_time = 1.0f;
+                printf("penalty time reduced to %.1f\n", penalty_time);
+                penalty_reduction_accumulator = 0.0f;
+            }
+        }
 
-			if (penalty_reduction_accumulator > 10.0f && penalty_time > 1.0f)
-			{
-				penalty_time /= 2.0f;
-				if (penalty_time < 1.0f)
-					penalty_time = 1.0f;
-				printf("penalty time reduced to %.1f\n", penalty_time);
-				penalty_reduction_accumulator = 0.0f;
-			}
-		}
+        if (mode == Bad)
+        {
+            if (rtt <= RTT_Threshold)
+                good_conditions_time += deltaTime;
+            else
+                good_conditions_time = 0.0f;
 
-		if (mode == Bad)
-		{
-			if (rtt <= RTT_Threshold)
-				good_conditions_time += deltaTime;
-			else
-				good_conditions_time = 0.0f;
+            if (good_conditions_time > penalty_time)
+            {
+                printf("*** upgrading to good mode ***\n");
+                good_conditions_time = 0.0f;
+                penalty_reduction_accumulator = 0.0f;
+                mode = Good;
+                return;
+            }
+        }
+    }
 
-			if (good_conditions_time > penalty_time)
-			{
-				printf("*** upgrading to good mode ***\n");
-				good_conditions_time = 0.0f;
-				penalty_reduction_accumulator = 0.0f;
-				mode = Good;
-				return;
-			}
-		}
-	}
-
-	float GetSendRate()
-	{
-		return mode == Good ? 30.0f : 10.0f;
-	}
+    float GetSendRate()
+    {
+        return mode == Good ? 30.0f : 10.0f;
+    }
 
 private:
+    enum Mode
+    {
+        Good,
+        Bad
+    };
 
-	enum Mode
-	{
-		Good,
-		Bad
-	};
-
-	Mode mode;
-	float penalty_time;
-	float good_conditions_time;
-	float penalty_reduction_accumulator;
+    Mode mode;
+    float penalty_time;
+    float good_conditions_time;
+    float penalty_reduction_accumulator;
 };
 
 // ----------------------------------------------
 
 int main(int argc, char* argv[])
 {
-	// parse command line
-	enum Mode
-	{
-		Client,
-		Server
-	};
+    // parse command line
+    enum Mode
+    {
+        Client,
+        Server
+    };
 
-	Mode mode = Server;
-	Address address;
-	string fileName;
-	
-	/*
-	Checks if we are running as a client or server.
-	When running as the client the system grabs file path we are sending and IP address of the server.
-	*/ 
-	if (argc >= 2)
-	{
-		int a, b, c, d;
+    Mode mode = Server;
+    Address address;
+    string fileName;
+
+    /*
+        Checks if we are running as a client or server.
+        When running as the client the system grabs file path we are sending and IP address of the server.
+    */
+    if (argc >= 2)
+    {
+        int a, b, c, d;
 #pragma warning(suppress : 4996)
-		if (sscanf(argv[1], "%d.%d.%d.%d", &a, &b, &c, &d))
-		{
-			mode = Client; 
-			address = Address(a, b, c, d, ServerPort); // grabs the server IP address
-			fileName = argv[2]; // grabs file path to the file we are sending. 
-		}
-	}
+        if (sscanf(argv[1], "%d.%d.%d.%d", &a, &b, &c, &d))
+        {
+            mode = Client;
+            address = Address(a, b, c, d, ServerPort); // grabs the server IP address
+            fileName = argv[2]; // grabs file path to the file we are sending. 
+        }
+    }
 
-	// initialize
-	if (!InitializeSockets())
-	{
-		printf("failed to initialize sockets\n");
-		return 1;
-	}
+    // initialize
+    if (!InitializeSockets())
+    {
+        printf("failed to initialize sockets\n");
+        return 1;
+    }
 
-	ReliableConnection connection(ProtocolId, TimeOut);
+    ReliableConnection connection(ProtocolId, TimeOut);
 
-	const int port = mode == Server ? ServerPort : ClientPort;
+    const int port = mode == Server ? ServerPort : ClientPort;
 
-	if (!connection.Start(port))
-	{
-		printf("could not start connection on port %d\n", port);
-		return 1;
-	}
+    if (!connection.Start(port))
+    {
+        printf("could not start connection on port %d\n", port);
+        return 1;
+    }
 
-	std::ifstream file;
-	if (mode == Client)
-	{
-		// opens the file, checks if it can even open
-		file.open(fileName, std::ios::binary);
-		if (!file)
-		{
-			// error opening the file
-			printf("Error: could not open \"%s\" Please try again.\n", fileName.c_str());
-			return 0;
-		}
-		else // file was opened successfully
-		{
-			// connects to the server
-			connection.Connect(address); 
+    // ------------------------------
+    // Client Side: Send Metadata
+    // ------------------------------
+    if (mode == Client)
+    {
+        std::ifstream file;
+        file.open(fileName, std::ios::binary);
+        if (!file)
+        {
+            // error opening the file
+            printf("Error: could not open \"%s\". Please try again.\n", fileName.c_str());
+            return 0;
+        }
+        else // file opened successfully
+        {
+            // connects to the server
+            connection.Connect(address);
 
-			// grabs the file size from the file metadata
-			file.seekg(0, std::ios::end);			// this moves the file pointer to the end of the file
-			std::streamsize fileSize = file.tellg();// this grabs the size of the file
-			file.seekg(0, std::ios::beg);			// moves file pointer back to start of file. (so we can send it).
-			// sends the file size to the server
-			connection.SendPacket(reinterpret_cast<unsigned char*>(&fileSize), sizeof(fileSize));
-			
-			// reads the file into a buffer and creates the MD5 hash
-			vector<char> fileBuffer(fileSize);       
-			file.read(fileBuffer.data(), fileSize);  
-			MD5 md5;
-			md5.update(fileBuffer.data(), fileSize);  
-			string fileHash = md5.finalize().hexdigest();
-			// sends the MD5 hash to the server
-			connection.SendPacket(reinterpret_cast<const unsigned char*>(fileHash.c_str()), fileHash.length());
-		}	
-	}
-		
-	else
-	{
-		connection.Listen();
-	}
-		
+            // grabs the file size from the file metadata
+            file.seekg(0, std::ios::end);              // this moves the file pointer to the end of the file
+            std::streamsize fileSize = file.tellg();   // this grabs the size of the file
+            file.seekg(0, std::ios::beg);              // moves file pointer back to start of file
+            // sends the file size to the server
+            connection.SendPacket(reinterpret_cast<unsigned char*>(&fileSize), sizeof(fileSize));
 
-	bool connected = false;
-	float sendAccumulator = 0.0f;
-	float statsAccumulator = 0.0f;
+            // reads the file into a buffer and creates the MD5 hash
+            vector<char> fileBuffer(fileSize);
+            file.read(fileBuffer.data(), fileSize);
+            MD5 md5;
+            md5.update(fileBuffer.data(), fileSize);
+            string fileHash = md5.finalize().hexdigest();
+            // sends the MD5 hash to the server
+            connection.SendPacket(reinterpret_cast<const unsigned char*>(fileHash.c_str()), fileHash.length());
 
-	FlowControl flowControl;
+            // sends the file name to the server
+            connection.SendPacket(reinterpret_cast<const unsigned char*>(fileName.c_str()), fileName.length());
 
-	while (true)
-	{
-		// update flow control
-		if (connection.IsConnected())
-			flowControl.Update(DeltaTime, connection.GetReliabilitySystem().GetRoundTripTime() * 1000.0f);
+            printf("Client sent metadata:\n");
+            printf("  File size: %lld bytes\n", (long long)fileSize);
+            printf("  MD5 hash: %s\n", fileHash.c_str());
+            printf("  File name: %s\n", fileName.c_str());
+        }
+    }
+    // ------------------------------
+    // Server Side: Receive Metadata
+    // ------------------------------
+    else // mode == Server
+    {
+        connection.Listen();
+        // wait until connected
+        while (!connection.IsConnected())
+        {
+            connection.Update(DeltaTime);
+            net::wait(DeltaTime);
+        }
 
-		const float sendRate = flowControl.GetSendRate();
+        // Variables to track metadata reception
+        bool fileSizeReceived = false;
+        bool hashReceived = false;
+        bool fileNameReceived = false;
+        std::streamsize fileSize = 0;
+        std::string fileHash;
+        std::string fileName;
 
-		// detect changes in connection state
-		if (mode == Server && connected && !connection.IsConnected())
-		{
-			flowControl.Reset();
-			printf("reset flow control\n");
-			connected = false;
-		}
+        // Loop until all metadata packets are received
+        while (!(fileSizeReceived && hashReceived && fileNameReceived))
+        {
+            // receive packets
+            unsigned char packet[PacketSize];
+            int bytes_read = connection.ReceivePacket(packet, sizeof(packet));
+            if (bytes_read > 0)
+            {
+                if (!fileSizeReceived)
+                {
+                    // first packet: file size (sent as binary)
+                    fileSize = *reinterpret_cast<std::streamsize*>(packet);
+                    printf("Received file size: %lld bytes\n", (long long)fileSize);
+                    fileSizeReceived = true;
+                }
+                else if (!hashReceived)
+                {
+                    // second packet: MD5 hash string
+                    fileHash = std::string(reinterpret_cast<char*>(packet), bytes_read);
+                    printf("Received MD5 hash: %s\n", fileHash.c_str());
+                    hashReceived = true;
+                }
+                else if (!fileNameReceived)
+                {
+                    // third packet: file name
+                    fileName = std::string(reinterpret_cast<char*>(packet), bytes_read);
+                    printf("Received file name: %s\n", fileName.c_str());
+                    fileNameReceived = true;
+                }
+            }
+            connection.Update(DeltaTime);
+            net::wait(DeltaTime);
+        }
+    }
 
-		if (!connected && connection.IsConnected())
-		{
-			printf("client connected to server\n");
-			connected = true;
-		}
+    ShutdownSockets();
 
-		if (!connected && connection.ConnectFailed())
-		{
-			printf("connection failed\n");
-			break;
-		}
-
-		// send and receive packets
-		sendAccumulator += DeltaTime;
-
-		// this is where the file is broken up into chunks and sent to the server.
-		while (sendAccumulator > 1.0f / sendRate)
-		{
-			//unsigned char packet[PacketSize];	    // Creates an empty packet of size `PacketSize`
-			// memset(packet, 0, sizeof(packet));	// Fills the packet with zeros
-			// connection.SendPacket(packet, sizeof(packet)); // Sends the empty packet over UDP
-			// sendAccumulator -= 1.0f / sendRate;	// Adjusts the accumulator for sending rate control
-			//file.read(reinterpret_cast<char*>(packet), PacketSize);
-			//connection.SendPacket(packet, file.gcount());
-
-			unsigned char packet[PacketSize];
-			file.read(reinterpret_cast<char*>(packet), PacketSize);
-			int bytesRead = file.gcount();
-			if (bytesRead > 0)
-			{
-				connection.SendPacket(packet, bytesRead);
-				sendAccumulator -= 1.0f / sendRate;
-			}
-			else
-			{
-				break; // no more data to send
-			}
-		}
-
-
-
-
-		// this is where the file metadata is recieved by the server
-		bool fileSizeReceived = false;
-		bool hashReceived = false;
-		while (true)
-		{
-			// receive packets
-			unsigned char packet[PacketSize];
-			int bytes_read = connection.ReceivePacket(packet, sizeof(packet));
-			if (bytes_read == 0)
-				break;  // no packet available
-
-			if (!fileSizeReceived)
-			{
-				// first packet: file size (sent as binary)
-				std::streamsize fileSize = *reinterpret_cast<std::streamsize*>(packet);
-				printf("Received file size: %lld bytes\n", (long long)fileSize);
-				fileSizeReceived = true;
-			}
-			else if (!hashReceived)
-			{
-				// second packet: MD5 hash string
-				std::string hash(reinterpret_cast<char*>(packet), bytes_read);
-				printf("Received MD5 hash: %s\n", hash.c_str());
-				hashReceived = true;
-			}
-		}
-
-
-#ifdef SHOW_ACKS
-		unsigned int* acks = NULL;
-		int ack_count = 0;
-		connection.GetReliabilitySystem().GetAcks(&acks, ack_count);
-		if (ack_count > 0)
-		{
-			printf("acks: %d", acks[0]);
-			for (int i = 1; i < ack_count; ++i)
-				printf(",%d", acks[i]);
-			printf("\n");
-		}
-#endif
-
-		// update connection
-
-		connection.Update(DeltaTime);
-
-		// show connection stats
-
-		statsAccumulator += DeltaTime;
-
-		while (statsAccumulator >= 0.25f && connection.IsConnected())
-		{
-			float rtt = connection.GetReliabilitySystem().GetRoundTripTime();
-
-			unsigned int sent_packets = connection.GetReliabilitySystem().GetSentPackets();
-			unsigned int acked_packets = connection.GetReliabilitySystem().GetAckedPackets();
-			unsigned int lost_packets = connection.GetReliabilitySystem().GetLostPackets();
-
-			float sent_bandwidth = connection.GetReliabilitySystem().GetSentBandwidth();
-			float acked_bandwidth = connection.GetReliabilitySystem().GetAckedBandwidth();
-
-			printf("rtt %.1fms, sent %d, acked %d, lost %d (%.1f%%), sent bandwidth = %.1fkbps, acked bandwidth = %.1fkbps\n",
-				rtt * 1000.0f, sent_packets, acked_packets, lost_packets,
-				sent_packets > 0.0f ? (float)lost_packets / (float)sent_packets * 100.0f : 0.0f,
-				sent_bandwidth, acked_bandwidth);
-
-			statsAccumulator -= 0.25f;
-		}
-
-		net::wait(DeltaTime);
-	}
-
-	ShutdownSockets();
-
-	return 0;
+    return 0;
 }
